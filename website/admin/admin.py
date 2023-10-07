df87a267ae292c65ad2bb1d1019d29f95604cf6b
Jack Ricketts <95173538+JLR24@users.noreply.github.com>
2023-10-07 19:43:20 +0100

    Added the Admin Dashboard.

--- website/admin/admin.py
+++ website/admin/admin.py
@@ -1,14 +1,100 @@
 from flask import Blueprint, render_template, url_for, redirect, flash, request
 from flask_login import current_user, login_required
-from ..models import db
-from .static.admin_utilities import IsAdmin
+from ..models import db, User
+from .static.admin_utilities import IsAdmin, GetAdmins, GetEditors, GetMax
 
 admin = Blueprint("admin", __name__, template_folder="templates", static_folder="static")
 
 
 @admin.route("/")
+@login_required
 def Home():
     '''This function displays the admin home page'''
     if not IsAdmin():
         return redirect(url_for("home.Home"))
-    return render_template("admin_home.html", user=current_user)
+    admins = GetAdmins()
+    editors = GetEditors()
+    return render_template("admin_home.html", user=current_user, active="Dashboard", m=GetMax([admins, editors]), admins=admins, editors=editors)
+
+
+@admin.route("/bans")
+@login_required
+def Bans():
+    '''This function displays the admin ban page'''
+    if not IsAdmin():
+        return redirect(url_for("home.Home"))
+    return render_template("admin_ban.html", user=current_user, active="Bans")
+
+
+@admin.route("/HandleAdminPromote", methods=["POST"])
+@login_required
+def HandleAdminPromote():
+    '''This page handles the promote new admin form submission'''
+    if not IsAdmin():
+        return redirect(url_for("home.Home"))
+    user = User.query.filter_by(username=request.form.get("username")).first()
+    if not user:
+        flash("Couldn't find a user with that name...", category="error")
+        return redirect(url_for("admin.Home"))
+    if user in GetAdmins():
+        return redirect(url_for("admin.Home"))
+    user.status = "Admin"
+    db.session.commit()
+    flash("User has been promoted!", category="success")
+    return redirect(url_for("admin.Home"))
+
+
+@admin.route("/HandleEditorPromote", methods=["POST"])
+@login_required
+def HandleEditorPromote():
+    '''This page handles the promote new editor form submission'''
+    if not IsAdmin():
+        return redirect(url_for("home.Home"))
+    user = User.query.filter_by(username=request.form.get("username")).first()
+    if not user:
+        flash("Couldn't find a user with that name...", category="error")
+        return redirect(url_for("admin.Home"))
+    if user in GetEditors():
+        return redirect(url_for("admin.Home"))
+    if user in GetAdmins():
+        flash("User is already an admin, cannot demote.", category="error")
+        return redirect(url_for("admin.Home"))
+    user.status = "Editor"
+    db.session.commit()
+    flash("User has been promoted!", category="success")
+    return redirect(url_for("admin.Home"))
+
+
+@admin.route("/HandleAdminDemote", methods=["POST"])
+@login_required
+def HandleAdminDemote():
+    '''This page handles the demote admin form submission'''
+    if not IsAdmin():
+        return redirect(url_for("home.Home"))
+    user = User.query.filter_by(username=request.form.get("username"), status="Admin").first()
+    if not user:
+        flash("Couldn't find an admin with that name...", category="error")
+        return redirect(url_for("admin.Home"))
+    if user.username == "JLR24":
+        flash("You can't demote JLR24...", category="error")
+        return redirect(url_for("admin.Home"))
+    user.status = "User"
+    db.session.commit()
+    flash("User has been demoted!", category="success")
+    return redirect(url_for("admin.Home"))
+
+
+@admin.route("/HandleEditorDemote", methods=["POST"])
+@login_required
+def HandleEditorDemote():
+    '''This page handles the demote editor form submission'''
+    if not IsAdmin():
+        return redirect(url_for("home.Home"))
+    user = User.query.filter_by(username=request.form.get("username"), status="Editor").first()
+    if not user:
+        flash("Couldn't find an editor with that name...", category="error")
+        return redirect(url_for("admin.Home"))
+    user.status = "User"
+    db.session.commit()
+    flash("User has been promoted!", category="success")
+    return redirect(url_for("admin.Home"))
--- website/admin/static/admin_utilities.py
+++ website/admin/static/admin_utilities.py
@@ -1,6 +1,6 @@
 from flask import flash
 from flask_login import current_user
-from ... import db
+from ...models import db, User
 
 
 def IsAdmin():
@@ -9,3 +9,22 @@ def IsAdmin():
         flash("You don't have permission to view this page.", category="error")
         return False
     return True
+
+
+def GetEditors():
+    '''Returns a list of all users with the rank "Editor"'''
+    return User.query.filter_by(status="Editor").all()
+
+
+def GetAdmins():
+    '''Returns a list of all users with the rank "Admin"'''
+    return User.query.filter_by(status="Admin").all()
+
+
+def GetMax(input):
+    '''Returns the maximum length of lists in the inputted list -> Handles "None"s.'''
+    m = 0
+    for i in input:
+        if i != None and len(i) > m:
+            m = len(i)
+    return m
