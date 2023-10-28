637b03edbbfe2db488d9acd44619cd13372b154d
Jack Ricketts <95173538+JLR24@users.noreply.github.com>
2023-10-28 21:01:07 +0100

    Implemented ranking swapping for genres.

--- website/mh/profile/static/genres.js
+++ website/mh/profile/static/genres.js
@@ -10,11 +10,25 @@ function isRankingFree(rank) {
     return true;
 }
 
+function compareRankings(a, b) {
+    // Source: https://javascript.plainenglish.io/how-to-sort-json-object-arrays-based-on-a-key-a157461e9610
+    if (a.ranking < b.ranking) {
+        return -1;
+    } else if (a.ranking > b.ranking) {
+        return 1;
+    }
+    return 0;
+}
+
 
 function setUpForm() {
     let genreBox = document.getElementById("genreBox");
+    genreBox.innerHTML = "";
     let count = 1;
-    for (g of window.genres) {
+
+    let genres = window.genres.sort(compareRankings);
+
+    for (g of genres) {
         let genreDiv = document.createElement("div");
         genreDiv.innerHTML = `
             <div class="input-group" style="border:1px dashed black; margin-bottom: 10px; border-radius: 5px;">
@@ -23,6 +37,7 @@ function setUpForm() {
                     <button type="button" class="arrowBtn" onclick="increaseRating(${count})">^</button>
                     <button type="button" class="arrowBtn" onclick="decreaseRating(${count})">v</button>
                 </div>
+                <input type="hidden" value="${g.id}" id="id:${g.id}" name="id:${g.id}">
                 <input type="text" value="${g.genre}" name="genre:{{g.id}}" id="genre:${count}" readonly class="form-control" style="background-color:white;border:none; text-align:center">
                 <input type="submit" value="Delete" name="delete:{{g.id}}" class="btn btn-outline-danger">
                 <input type="submit" value="Update" name="update:{{g.id}}" class="btn btn-outline-success">
@@ -34,9 +49,29 @@ function setUpForm() {
 }
 
 function increaseRating(n) {
-    console.log(parseInt(document.getElementById("ranking:" + n).value) - 1);
+    if (n == 1) {
+        return;
+    }
+
+    let genres = window.genres.sort(compareRankings);
+    console.log(genres);
+    console.log(n);
+    genres[n - 2].ranking += 1;
+    genres[n - 1].ranking -= 1;
+    setUpForm();
+    // MAKE SURE TO UPDATE IDs AS WELL (then: allow the user to add new genres, then save on the backend using a for loop (make sure to update the "count" hidden form input))
 }
 
 function decreaseRating(n) {
-    console.log(parseInt(document.getElementById("ranking:" + n).value) + 1);
+    let genres = window.genres.sort(compareRankings);
+    if (n == genres.length) {
+        return;
+    }
+
+    console.log(genres);
+    console.log(n);
+    genres[n - 1].ranking += 1;
+    genres[n].ranking -= 1;
+    setUpForm();
+    // MAKE SURE TO UPDATE IDs AS WELL (then: allow the user to add new genres, then save on the backend using a for loop (make sure to update the "count" hidden form input))
 }
